1312a89bd00d0f3077644b21ae82c6e1ae51d482
batman-drophouse <batman@drophouse.ai>
2024-08-27 10:08:52 +0530

    error page rebase

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './ErrorPage.css'; // Make sure to link the CSS file
 import { useLocation } from 'react-router-dom';
+import errorImg from '../assets/500-error.png'
 
 const ErrorPage = () => {
   const location = useLocation();
