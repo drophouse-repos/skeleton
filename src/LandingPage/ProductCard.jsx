2dee198133de75d22d44c486926316b13566ad93
Selvam43 <muthuselvam.m99@gmail.com>
2024-08-30 15:54:27 +0530

    Landing Page Slider card UI changed

--- src/LandingPage/ProductCard.jsx
+++ src/LandingPage/ProductCard.jsx
@@ -22,35 +22,46 @@ export default function ProductCard({ product, changeInterval, type }) {
         return <img className="landing-random-image" src={source} alt={`${name} - ${RandomImageUpdateCount}`} />;
     }
 
-    function SequentialImageCarousel({ imageList, name }) {
+    function SequentialImageCarousel({ imageList_front,imageList_back, name }) {
 
-        let source = imageList[SequentialImageUpdateCount % imageList.length];
-        return <img className="landing-random-image" src={source} alt={`${name} - ${SequentialImageUpdateCount}`} />;
+        let source_front = imageList_front[SequentialImageUpdateCount % imageList_front.length];
+        let source_back = imageList_back[SequentialImageUpdateCount % imageList_back.length];
+        // return (<img className="landing-random-image" src={source} alt={`${name} - ${SequentialImageUpdateCount}`} />);
+        return (
+            <div className="image-container-main ">
+            <div className="image-container">
+              <img className="landing-random-image" src={source_front} alt={`${name} - ${SequentialImageUpdateCount}`} />
+              <img className="back-img" src={source_back ? source_back : source_front} alt={`${name} - ${SequentialImageUpdateCount}`} />
+            </div>
+            </div>
+          );
+          
     }
 
     return (
-        <div className="flex flex-col items-center justify-center h-full">
-            <div className="flex flex-col h-full items-center justify-center my-2 py-2 rounded-[20px] bg-gradient-to-br from-[#FFFFFF] to-[#FFFEF2] mx-3 pb-5">
-                <div className="w-4/5 flex flex-col h-full items-center">
+        <div className="flex flex-col items-center justify-center card-product-slider h-full">
+            <div className="flex flex-col w-11/12 h-full items-center justify-center my-2 py-2 rounded-[10px] bg-white rounder-md mx-3 pb-0">
+                <div className="w-full flex flex-col h-full items-center">
                     <div className="flex justify-center h-full">
                         <SequentialImageCarousel
-                            imageList={product.imageList}
+                            imageList_front={product.imageList_front}
+                            imageList_back={product.imageList_back}
                             name={product.name}
                         />
                     </div>
 
 
-                    <div className="flex flex-col justify-center items-center w-full rounded-[20px] bg-white px-2 mx-5 mt-auto">
-                        <div style={{fontFamily : `${orgDetails[0].font}`}} className={`tracking-wide text-gray-900 font-bold my-3`}>
+                    <div className="flex flex-col  text-left bg-white justify-center items-center w-full rounded-t-none rounded-md pt-0  pb-0 py-2.5 m-0 mt-auto">
+                        <h2 style={{fontFamily : `${orgDetails[0].font}`,display: 'flex',width: '100%',justifyContent:'center',paddingLeft: '6px'}} className={`tracking-wide text-gray-600 font-bold m-0`}>
                             {product.name}
-                        </div>
+                        </h2>
 
-                        <div style={{fontFamily : `${orgDetails[0].font}`}} className={`text-bold tracking-wide text-xl text-black font-extrabold`}>
+                        <h2 style={{fontFamily : `${orgDetails[0].font}`,display: 'flex',width: '100%',justifyContent:'center',paddingLeft: '6px'}} className={`text-bold text-left tracking-wide text-xl text-black font-extrabold`}>
                             ${product.price.toFixed(2)}
-                        </div>
+                        </h2>
 
-                        <div className="flex justify-center items-center place-self-center pt-2 w-full">
-                            <GoToProductButton text="Design Now" link={"/product"} className={`text-white rounded-[20px] px-5`} type={product.type} color={product.color} />
+                        <div className="flex justify-left items-center place-self-center  pb-0 pt-2 w-full">
+                            <GoToProductButton text="Design Now" link={"/product"} className={`rounded-t-none  buy-btn-slider text-white w-full rounded-[5px] px-16 py-2.5`} type={product.type} color={product.color} />
                         </div>
                     </div>
                 </div>
