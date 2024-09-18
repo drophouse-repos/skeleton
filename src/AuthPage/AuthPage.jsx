1d8a8dd246e1a21edd437f10701442082c261e3b
Kush Bhuwalka <kushbhuwalka@gmail.com>
2024-09-18 15:56:15 -0500

    window locatoin added

--- src/AuthPage/AuthPage.jsx
+++ src/AuthPage/AuthPage.jsx
@@ -86,9 +86,8 @@ const AuthPage = () => {
 
   const sendMagicLink = async (email) => {
     try {
-      console.log(window.location.origin)
       const actionCodeSettings = {
-        url: process.env.REACT_APP_FRONTEND + "/auth",
+        url: `${window.location.origin}/auth`,
         handleCodeInApp: true,
       };
       await sendSignInLinkToEmail(auth, email, actionCodeSettings);
