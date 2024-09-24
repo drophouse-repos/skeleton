5ac59fcd0a9bcb494c276047f90b9de029b7e331
batman-drophouse <batman@drophouse.ai>
2024-09-24 14:39:23 +0530

    Demo Site Changes

---
+++ src/context/DemoContext.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './DemoOverlay.css'; // Import the CSS for styling
+const DemoOverlay = () => {
+    return (
+        <div className="demo-overlay">
+            <p style={{fontSize:'40px'}}>Demo site</p>
+            <p>For Reference Only</p>
+        </div>
+    );
+};
+export default DemoOverlay;
