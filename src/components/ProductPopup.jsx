b39c36c7e733381c329b09c0ec49599df6f74237
batman-drophouse <batman@drophouse.ai>
2024-09-25 10:04:30 +0530

    modal size changed

--- src/components/ProductPopup.jsx
+++ src/components/ProductPopup.jsx
@@ -18,7 +18,7 @@ export default function ProductPopup({ productInfo, popupTitle, isShown, setIsSh
     return (
         isShown && (<>
         {isSaveDesign ? 
-                <div className="flex flex-col space-t-3 fixed bg-neutral-100 rounded-lg top-16 right-10 w-10/12 h-4/12 md:w-6/12 md:h-3/12 lg:w-4/12 lg:h-2/12 z-50 shadow-[0_0px_10px_3px_rgba(0,0,0,0.3)]">
+                <div  className="flex flex-col space-t-3 fixed bg-neutral-100 rounded-lg top-16 right-10 z-50 shadow-[0_0px_10px_3px_rgba(0,0,0,0.3)]">
                 <div className="flex flex-row relative justify-center items-center content-center">
                     <div className="pt-2 font-bold " style={{fontFamily: 'Arsenal'}}>{popupTitle}</div>
                     <div className="absolute right-2 hover:text-orange-500">
