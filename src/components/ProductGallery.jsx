5f0e75903b08caf8906766a9c313d65af8130036
batman-drophouse <batman@drophouse.ai>
2024-09-25 10:08:29 +0530

    rebased

--- src/components/ProductGallery.jsx
+++ src/components/ProductGallery.jsx
@@ -19,7 +19,6 @@ import { enhanceImageClarity } from '../utils/enhanceImageClarity';
 import { Orgcontext } from '../context/ApiContext';
 import ZoomIcon from '../assets/zoom.png';
 
-import ProductPopup from "./ProductPopup";
 const ProductGallery = forwardRef(({ onChange, setToggled, setToggleActivated, currentIndex, setCurrentIndex, changeFromMug, isZoomEnabled, setIsZoomEnabled }, ref) => {
 
   const { apparel, setApparel, color, setColor, prompt } = useContext(AppContext);
