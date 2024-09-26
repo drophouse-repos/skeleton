180449538e78e90feb6911140959a2e5624707d4
batman-drophouse <batman@drophouse.ai>
2024-09-26 12:46:56 +0530

    KAN-112-add-a-env-called-react-app-demo-implement

--- src/InformationPage/InformationPage.jsx
+++ src/InformationPage/InformationPage.jsx
@@ -81,13 +81,14 @@ const InformationPage = () => {
           }, {});
           setCountryMap(countryData)
       } catch {
-        console.error("error fetching country list")
+        console.log("error fetching country list")
       }
     }
     fetchCountryList();
   },[])
   const [stateList, setStateList] = useState([])
   const handleCountryChange = async (selectedoption,changestate) => {
+    // console.log(countryMap[selectedoption])
     if(changestate){
       handleModalInputChange('state',null)
     }
@@ -100,7 +101,7 @@ const InformationPage = () => {
       }))
       setStateList(stateListData)
     } catch(err) {
-      console.error("error fetching state list : ",err);
+      console.log("error fetching state list : ",err);
     }
   }
   
@@ -268,6 +269,7 @@ const InformationPage = () => {
     productInfo["shipping_info"] = findAddress('primary');
     productInfo["org_id"] = orgDetails.org_id;
     productInfo["org_name"] = orgDetails.name;
+    console.log(productInfo)
     if(productInfo['shipping_info'] && productInfo['shipping_info']['firstName'] && productInfo['shipping_info']['email'] && productInfo['shipping_info']['lastName'] && productInfo['shipping_info']['phone'] && productInfo['shipping_info']['postalZipcode'] && productInfo['shipping_info']['stateProvince'] && productInfo['shipping_info']['streetAddress'] && productInfo['shipping_info']['city'])
     {
     if(process.env.REACT_APP_STRIPE_CHECKOUT_ENABLED === "false")
@@ -362,15 +364,19 @@ const InformationPage = () => {
       if (types.includes('country')) {
         handleModalInputChange('country', short_name);
         handleCountryChange(short_name, false)
+        console.log("Country : ",long_name)
       }
       if (types.includes('administrative_area_level_3') || types.includes('locality')) {
         handleModalInputChange('city', long_name);
+        console.log("city : ",long_name)
       } 
       if (types.includes('administrative_area_level_1')) {
         handleModalInputChange('state', long_name);
+        console.log("state : ",long_name)
       } 
       if (types.includes('postal_code')) {
         handleModalInputChange('zipCode', long_name);
+        console.log("zipcode : ",long_name)
       }
     });
   };
@@ -390,6 +396,15 @@ const InformationPage = () => {
     // Return the icon name from the map or a default icon
     return iconMap[field] || 'help'; // 'help' is a fallback icon
   };
+  const [DemoModal, setDemoModal] = useState(false);
+  const ShowDemoModal = (e) => {
+    e.preventDefault();
+    setDemoModal(!DemoModal);
+  }
+  const CloseShowDemoModal = (e) => {
+    e.preventDefault();
+    setDemoModal(!DemoModal);
+  }
   
   return (
     <div>
@@ -534,10 +549,27 @@ const InformationPage = () => {
           </label>
         </div>
         {process.env.REACT_APP_DEMO ? 
-        <><button className="continueBtn" disabled={true} style={{fontFamily : `${orgDetails.font}`, color: `white`,backgroundColor: 'lightgrey'}}>
+        <><button className="continueBtn" onClick={(e) => ShowDemoModal(e)} style={{fontFamily : `${orgDetails.font}`, color: `${orgDetails.theme_color}`}}>
           ORDER
         </button><br></br>
-        <p>You can't place order Please contact : <a href="https://www.drophouse.ai/contact.php" style={{color:'red'}} target='_blank'>drophouse.ai</a></p></>
+        {DemoModal && 
+        <>
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50" style={{zIndex: `9999`}}>
+              <div className="bg-white p-4 rounded shadow-lg" style={{width: window.innerWidth <= 544 ? '90%' :'50%'}}>
+                  <h2 className="text-lg font-bold mb-4">This is a demo site for more details contact : <br></br>
+                  <span style={{color: `${orgDetails.theme_color}`}}> support@drophouse.art</span></h2>
+                  <div className="flex justify-end mt-4">
+                      <button
+                          className="bg-green-500 hover:bg-green-700 text-white py-1 px-3 rounded mr-2"
+                          onClick={(e) => CloseShowDemoModal(e)}
+                      >
+                          Ok
+                      </button>
+                  </div>
+              </div>
+          </div>
+        </>}
+        </>
         : 
         <button className="continueBtn" type="submit" disabled={isLoading} style={{fontFamily : `${orgDetails.font}`, color: `${orgDetails.theme_color}`}}>
           {isLoading ? (
