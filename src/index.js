c3b3a7714560bda314f081c25570db041e3dd101
batman-drophouse <batman@drophouse.ai>
2024-10-17 17:12:47 +0530

    api call twice in production server fixed

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const isLocalhost = process.env.REACT_APP_BACKBONE.includes('localhost') || process.env.REACT_APP_BACKBONE.includes('127.0.0.1');
+
 root.render(
-  <React.StrictMode>
+  isLocalhost ? (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Analytics />
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  ) : (
     <BrowserRouter>
-    <Analytics/>
+      <Analytics />
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-  
+  )
 );
